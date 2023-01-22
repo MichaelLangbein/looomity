2a3912b61dcbcc8913d283f40912f30ccfb477c9
Michael Langbein <michael_langbein@hotmail.de>
2023-01-22 20:01:46 +0100

    started work on unit-tests

--- LoomityUITests/LoomityUITests.swift
+++ LoomityUITests/LoomityUITests.swift
@@ -39,24 +39,23 @@ final class LoomityUITests: XCTestCase {
         }
     }
     
-    func testFullPass() throws {
-        let app = XCUIApplication()
-        app.launch()
-        
-        app.buttons["Select image"].tap()
-        app.buttons["Gallery"].tap()
-        app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Screenshot, 22. January, 08:50"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Screenshot, 22. January, 08:50, Live Photo, 21. January, 16:06, Live Photo, 21. January, 16:06, Photo, 21. January, 13:51, Screenshot, 20. January, 17:34, Screenshot, 20. January, 17:33, Screenshot, 19. January, 22:04, Screenshot, 19. January, 21:59, Photo, 19. January, 19:00, Photo, 19. January, 18:05, Photo, 15. January, 14:47, Live Photo, 15. January, 10:38, Live Photo, 15. January, 10:38, Live Photo, 13. January, 13:42, Live Photo, 12. January, 16:03\"].images[\"Screenshot, 22. January, 08:50\"]",".images[\"Screenshot, 22. January, 08:50\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
-        app.buttons["Loomify"].tap()
-        app.alerts["Couldn't detect any faces in your image."].scrollViews.otherElements.buttons["Continue"].tap()
-        app.buttons["Add model"].tap()
-        
-        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
-        element.tap()
-        element.tap()
-        element.tap()
-        element/*@START_MENU_TOKEN@*/.swipeLeft()/*[[".swipeDown()",".swipeLeft()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-        element.swipeRight()
-        element.tap()
-
-    }
+//    func testFullPass() throws {
+//        let app = XCUIApplication()
+//        app.launch()
+//        
+//        app.buttons["Select image"].tap()
+//        app.buttons["Gallery"].tap()
+//        app/*@START_MENU_TOKEN@*/.scrollViews.otherElements.images["Screenshot, 22. January, 08:50"]/*[[".otherElements[\"Photos\"].scrollViews.otherElements",".otherElements[\"Screenshot, 22. January, 08:50, Live Photo, 21. January, 16:06, Live Photo, 21. January, 16:06, Photo, 21. January, 13:51, Screenshot, 20. January, 17:34, Screenshot, 20. January, 17:33, Screenshot, 19. January, 22:04, Screenshot, 19. January, 21:59, Photo, 19. January, 19:00, Photo, 19. January, 18:05, Photo, 15. January, 14:47, Live Photo, 15. January, 10:38, Live Photo, 15. January, 10:38, Live Photo, 13. January, 13:42, Live Photo, 12. January, 16:03\"].images[\"Screenshot, 22. January, 08:50\"]",".images[\"Screenshot, 22. January, 08:50\"]",".scrollViews.otherElements"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+//        app.buttons["Loomify"].tap()
+//        app.alerts["Couldn't detect any faces in your image."].scrollViews.otherElements.buttons["Continue"].tap()
+//        app.buttons["Add model"].tap()
+//        
+//        let element = app.children(matching: .window).element(boundBy: 0).children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element
+//        element.tap()
+//        element.tap()
+//        element.tap()
+//        element/*@START_MENU_TOKEN@*/.swipeLeft()/*[[".swipeDown()",".swipeLeft()"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+//        element.swipeRight()
+//        element.tap()
+//    }
 }
